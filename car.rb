5de4dac08ebe63e9024b6d3f65fae8afa493d32d
chitrasrinivasan12 <csriniv@ncsu.edu>
2018-07-27 15:00:52 +0000

    automatically backed up by learn

---
+++ car.rb
@@ -0,0 +1,19 @@
+class Car 
+  def initialize car(model, make, year)
+    @carmodel = model 
+    @carmake = make 
+    @caryear = year
+  end 
+  
+  def model 
+    @carmodel
+  end 
+  
+  def make
+    @carkame
+  end 
+  
+  def year
+    @caryear
+  end 
+  
